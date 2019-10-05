f0eef4629d2fd4327f45c11c3733e2ba7b87f2d2
astridjerald <f20150082@dubai.bits-pilani.ac.in>
2019-10-05 20:42:12 +0400

    Add name and website link onto site

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,6 +7,7 @@ class Landing extends Component {
         <div className="jumbotron mt-5">
           <div className="col-sm-8 mx-auto">
             <h2 className="text-center">REACT-FLASK TODO APP</h2>
+            <p className="text-center">BY <a href="https://astridjerald.com">ASTRID JERALD</a></p>
           </div>
         </div>
       </div>
